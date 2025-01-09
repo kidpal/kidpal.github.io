6e09bb2148e5b265258e7cc1b2e5503a58cb21f3
kvh1029-kidpal <kvh1029@kidpal.com.tw>
2025-01-09 16:02:43 +0800

    Update update_bt_bin.sh

--- Bluez5.5/update_bt_bin.sh
+++ Bluez5.5/update_bt_bin.sh
@@ -4,30 +4,50 @@ sync
 if [ "`ls /customer/bluetooth/bin/bluetoothd`" == "" ]; then
 	echo "/customer/bluetooth/bin/bluetoothd is deleted"
 fi
+
 echo " "
 rm -rf /customer/bluetooth/bin/bluetoothctl
 sync
 if [ "`ls /customer/bluetooth/bin/bluetoothctl`" == "" ]; then
 	echo "/customer/bluetooth/bin/bluetoothctl is deleted"
 fi
+
+echo " "
+rm -rf /customer/bluetooth/bin/btgatt-server
+sync
+if [ "`ls /customer/bluetooth/bin/btgatt-server`" == "" ]; then
+	echo "/customer/bluetooth/bin/btgatt-server is deleted"
+fi
+
 echo " "
 if [ -e "/mnt/mmc/bluetoothctl" ]; then
 	cp /mnt/mmc/bluetoothctl /customer/bluetooth/bin/
 	sync
 	chmod a+rx /customer/bluetooth/bin/bluetoothctl
 	sync
+	ls -al /customer/bluetooth/bin/bluetoothctl
 	echo "bluetoothctl is copied to /customer/bluetooth/bin/"
 fi
+
 echo " "
 if [ -e "/mnt/mmc/bluetoothd" ]; then
 	cp /mnt/mmc/bluetoothd /customer/bluetooth/bin/
 	sync
 	chmod a+rx /customer/bluetooth/bin/bluetoothd
 	sync
+	ls -al /customer/bluetooth/bin/bluetoothd
 	echo "bluetoothd is copied to /customer/bluetooth/bin/"
+	
 fi
+
 echo " "
-ls -al /customer/bluetooth/bin/bluetoothd
-ls -al /customer/bluetooth/bin/bluetoothctl
+if [ -e "/mnt/mmc/btgatt-server" ]; then
+	cp /mnt/mmc/btgatt-server /customer/bluetooth/bin/
+	sync
+	chmod a+rx /customer/bluetooth/bin/btgatt-server
+	sync
+	ls -al /customer/bluetooth/bin/btgatt-server
+	echo "btgatt-server is copied to /customer/bluetooth/bin/"
+fi
+
 echo " "
-echo "Successfully updated bluetoothd and bluetoothctl."
